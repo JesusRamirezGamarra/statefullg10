e68b04ca943e50aff846f1c1f2d008eba874c956
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2024-11-02 10:01:28 -0500

    Push 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,6 +24,35 @@ class HomePage extends StatelessWidget {
                 fontSize: 18,
                 fontWeight: FontWeight.w500,
               ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              //color:Colors.red,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(20),
+                border : Border.all(
+                  color: Colors.black,
+                  width: 2
+                )
+              ),
+              child: Row(
+                children: [
+                  Image.network(
+                    'https://picsum.photos/250?image=1',
+                    width: MediaQuery.of(context).size.width/3,
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Menu 1"),
+                        Text("Lun-Mar-Mier"),
+                        Text("S/ 5."),
+                      ],
+                  )
+                ]
+              ),
             )
           ],
       ),
